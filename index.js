7a1920058e186489aa3e6b87bd8e97bf77eb8cd3
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-01-30 08:42:53 +0900

    Create

---
+++ index.js
@@ -0,0 +1,15 @@
+var raspTime = [
+  'https://www.sakha.gov.ru',
+  'https://www.e-yakutia.ru/bs/main.htm',
+  'http://dom.e-yakutia.ru'
+ ];
+
+setInterval(checkRasp, 180000);
+
+function sendMessage(){
+  https://api.telegram.org/bot775773770:AAFKmqPkw4MgOhSPjzdxFjG_NRxjnLZXbmY/sendMessage?chat_id=337277275&text=Проблемы%20с%20'.$val
+}
+
+function checkGov(){
+
+}
