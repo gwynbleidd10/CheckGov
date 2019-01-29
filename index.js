154a97ad0e0090d62b4812339f2fe593cb38a73d
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-01-30 08:54:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var raspTime = [
+const request = require('request');
+
+var url = [
   'https://www.sakha.gov.ru',
   'https://www.e-yakutia.ru/bs/main.htm',
   'http://dom.e-yakutia.ru'
  ];
 
-setInterval(checkRasp, 180000);
+//setInterval(checkGov, 180000);
 
 function sendMessage(){
   https://api.telegram.org/bot775773770:AAFKmqPkw4MgOhSPjzdxFjG_NRxjnLZXbmY/sendMessage?chat_id=337277275&text=Проблемы%20с%20'.$val
 }
 
-function checkGov(){
-
+function checkGov(){  
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 }
