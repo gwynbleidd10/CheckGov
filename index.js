3efaa1abb112e6c5fcbe94a621606170dd88e983
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-01-30 09:23:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sendMessage(){
 checkGov();
   
 function checkGov(){  
-  request('http://www.google.com', function (error, response, body) {
+  request('http://minare0.pro', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   });
