2cc3c3046af69377dc84bcda77bec1b2704decfb
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-02-02 12:02:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request');
 const TelegramBot = require('node-telegram-bot-api');
 const token = '775773770:AAFKmqPkw4MgOhSPjzdxFjG_NRxjnLZXbmY';
 const bot = new TelegramBot(token, {polling: true});
-const curl = new (require( 'curl-request' ))();
+const Curl = require( 'curl-request' );
 
 var url = [
   'https://www.sakha.gov.ru',
@@ -33,6 +33,7 @@ function checkGov(){
       }
     });
   });
+  const curl = new Curl;
   curl.setHeaders([
     'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
   ])
