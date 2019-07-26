67004283edee22abcd204f9fd994f03633303f4e
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-07-27 05:57:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,27 @@
 process.env.NTBA_FIX_319 = 1;
+const ping = require('node-http-ping')
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
-const request = require('request');
-const Curl = require( 'curl-request' );
+var url = ['sakha.gov.ru', 'e-yakutia.ru', 'dom.e-yakutia.ru']
 
-var service = false;
-var time = [0, 0, 0];
-var err = [0, 0, 0];
-
-bot.sendMessage('337277275', 'Бот запущен!');
-console.log('Запуск');
-/*bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});*/
-
-bot.onText(/\/service/, function (msg) {
-  service = !service;
-  bot.sendMessage(msg.from.id, 'Service успешно установлен в: ' + service);
-  console.log("Service: " + service);
+bot.onText(/\/status/, function (msg) {
+    bot.sendMessage(msg.chat.id, 'Статус сайтов:' + pingCheck());
 });
 
-var url = [
-  'https://www.sakha.gov.ru',
-  'https://www.e-yakutia.ru/bs/main.htm'  
-];
-
-checkGov();
-
-function getTime() {
-  return new Date().toLocaleDateString('ru', {timeZone: 'Asia/Yakutsk', hour: 'numeric', minute: 'numeric'});
-}
-
-function sendMessage(status, site, id){
-  if (status == false) {
-    bot.sendMessage('337277275', time[id] + ' | Потеряно соединение с ' + site);
-  } else {
-    bot.sendMessage('337277275', getTime() + ' | Восстановлено соединение с ' + site);
-  }
-  console.log("Отправлено сообщение");
-}
-  
-function checkGov(){
-  
-  if (service == false) {
-    url.forEach(function(item, i, url) {
-      request(item, {timeout: 10000}, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          if (err[i] != 0){
-            err[i] = 0;
-            sendMessage(true, item, i)
-          }
-          console.log("OK : " + item);
-        } else {
-          if (error == 'Error: ETIMEDOUT'){
-          } else {
-            if (err[i] == 0) {
-            time[i] = getTime();
-            err[i] = 1;
-            sendMessage(false, item, i);
-            }
-            console.log('ERROR : ' + error + ' | ' + item);
-          }
-        }
-      });
+function pingCheck(){
+    result = '';
+    url.forEach(function(item){
+        ping(item, 80)
+        .then(time => {
+            console.log(`${item} time: ${time}ms`);
+            result += `\n${item} time: ${time}ms`;
+        })
+        .catch(() => {
+            console.log(`Failed to ping ${item}`);
+            result += `\nFailed to ping ${item}`;
+        })
     });
-    const curl = new Curl;
-    curl.setHeaders([
-      'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-    ])
-    .get('http://dom.e-yakutia.ru')
-    .then(({statusCode, body, headers}) => {
-      if (statusCode == 200) {
-        if (err[2] != 0){
-          err[2] = 0;
-          sendMessage(true, 'http://dom.e-yakutia.ru', 2)
-        }
-        console.log("OK : " + 'http://dom.e-yakutia.ru');
-      } else {
-        if (err[2] == 0) {
-          time[2] = getTime();
-          err[2] = 1;
-          sendMessage(false, 'http://dom.e-yakutia.ru', 2);
-        }
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-      if (err[2] == 0) {
-        time[2] = getTime();
-        err[2] = 1;
-        sendMessage(false, 'http://dom.e-yakutia.ru', 2);
-      }
-    });
-    console.log("------------------------------------");  
-  }
-};
-
-setInterval(checkGov, 300000);
+    return result;
+}
