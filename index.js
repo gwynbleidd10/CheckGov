1dbd4a5f6f7fe109af524224c1e869493ff39406
gwynbleidd10 <gwynbleidd10@gmail.com>
2019-07-27 20:40:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,47 @@
-//process.env.NTBA_FIX_319 = 1;
-//const ping = require('node-http-ping')
-//const TelegramBot = require('node-telegram-bot-api');
-//const token = process.env.BOT_TOKEN;
-//const bot = new TelegramBot(token, {polling: true});
+process.env.NTBA_FIX_319 = 1;
+const ping = require('node-http-ping')
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
 
 var url = ['sakha.gov.ru', 'e-yakutia.ru', 'dom.e-yakutia.ru', 'max.0code.pro'];
-var ms = [0, 0, 0];
-/*
-//bot.onText(/\/status/, function (msg) {
-    status();
-    var result = 'Статус сайтов:';
+var ms = [0, 0, 0, 0];
+
+console.log("Бот запущен!");
+
+bot.onText(/\/status/, function (msg) {
+    status();    
+    console.log(ms);
+    var str = 'Статус сайтов:';  
+});
+
+function strConfirm(){    
     ms.forEach(function(item, i){
-        console.log(i);
         if (item != '0'){
-            console.log(`\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`);
-            result.concat(`\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`);
+            str += `\n<a href=\"https://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`;
         }
         else
         {
-            result += `\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>Не овечает</code>`;
+            str += `\n<a href=\"https://${url[i]}/\">${url[i]}</a> - <code>Не овечает</code>`;
         }
-    });    
-    setTimeout(function(){        
-        console.log(result);
-    }, 1000);    
-    //bot.sendMessage(msg.chat.id, result, {parse_mode : "HTML"});
-//});
+    });
+    bot.sendMessage(msg.chat.id, str, {parse_mode : "HTML"});
+    console.log(str);
+}
 
-
-*/
-
-https://www.npmjs.com/package/is-reachable
-status();
-
-function status(){  
-    /*url.forEach(function(item, i){
-        ping(item, 80)
+async function status(){
+    for (item of url) {
+        await ping("https://" + item)
         .then(time => {
-            console.log(i);
-            console.log(`${item} time: ${time}ms`);
-            ms[i] = time;            
+            console.log(`${item} time: ${time}ms`);       
+            ms[url.indexOf(item)] = time;  
         })
         .catch(() => {
             console.log(`Failed to ping ${item}`);
-            ms[i] = 0;
-        });
-    }); */
-    const isReachable = require('is-reachable');
-    s = 0;
-    (async () => {        
-        s = Number(await isReachable('sindresorhus.com'));
-        console.log(s);
-        //=> true
-     
-        console.log(await isReachable('google.com:80'));
-        //=> true
-        console.log(s);
-    })();
-    console.log(s);
+            ms[url.indexOf(item)] = 0;  
+        });        
+    }
+    strConfirm();
 }
 
 //setInterval(pingCheck, 5000);
-
-
