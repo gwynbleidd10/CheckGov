5d73a0ce3414bd110e64054bfbf52617f0cb6d69
Gwynbleidd10 <gwynbleidd10@gmail.com>
2019-07-27 09:45:06 +0900

    test

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-process.env.NTBA_FIX_319 = 1;
-const ping = require('node-http-ping')
-const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token, {polling: true});
+//process.env.NTBA_FIX_319 = 1;
+//const ping = require('node-http-ping')
+//const TelegramBot = require('node-telegram-bot-api');
+//const token = process.env.BOT_TOKEN;
+//const bot = new TelegramBot(token, {polling: true});
 
 var url = ['sakha.gov.ru', 'e-yakutia.ru', 'dom.e-yakutia.ru', 'max.0code.pro'];
 var ms = [0, 0, 0];
-
-bot.onText(/\/status/, function (msg) {
+/*
+//bot.onText(/\/status/, function (msg) {
     status();
-    setTimeout(function(){
-        var result = 'Статус сайтов:';
-        ms.forEach(function(item, i){
-            if (item != '0'){
-                result += `\n<a href=\"https://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`;
-            }
-            else
-            {
-                result += `\n<a href=\"https://${url[i]}/\">${url[i]}</a> - <code>Не овечает</code>`;
-            }
-        });    
-    }, 1000);
-    bot.sendMessage(msg.chat.id, result, {parse_mode : "HTML"});
-});
+    var result = 'Статус сайтов:';
+    ms.forEach(function(item, i){
+        console.log(i);
+        if (item != '0'){
+            console.log(`\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`);
+            result.concat(`\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>${item}ms</code>`);
+        }
+        else
+        {
+            result += `\n<a href=\"http://${url[i]}/\">${url[i]}</a> - <code>Не овечает</code>`;
+        }
+    });    
+    setTimeout(function(){        
+        console.log(result);
+    }, 1000);    
+    //bot.sendMessage(msg.chat.id, result, {parse_mode : "HTML"});
+//});
 
 
+*/
 
+https://www.npmjs.com/package/is-reachable
 status();
 
 function status(){  
-    url.forEach(function(item, i){
+    /*url.forEach(function(item, i){
         ping(item, 80)
         .then(time => {
+            console.log(i);
             console.log(`${item} time: ${time}ms`);
             ms[i] = time;            
         })
@@ -39,11 +45,21 @@ function status(){
             console.log(`Failed to ping ${item}`);
             ms[i] = 0;
         });
-    });    
+    }); */
+    const isReachable = require('is-reachable');
+    s = 0;
+    (async () => {        
+        s = Number(await isReachable('sindresorhus.com'));
+        console.log(s);
+        //=> true
+     
+        console.log(await isReachable('google.com:80'));
+        //=> true
+        console.log(s);
+    })();
+    console.log(s);
 }
 
 //setInterval(pingCheck, 5000);
-
-
 
 
